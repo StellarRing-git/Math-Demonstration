f2bfb2be6a35f26978e1d3f7db824cfddd089deb
StellarRing-git <149808896+StellarRing-git@users.noreply.github.com>
2023-11-21 22:45:57 +0530

    bug fixes again

--- engine V2.py
+++ engine V2.py
@@ -1,13 +1,17 @@
 import numpy as np
 import pygame
 import pyglet
+import math
 
 
 
 class Game():
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode((900,600))
+        self.window = pygame.display.set_mode((1366,768))
+        self.background = pygame.image.load('background.jpg').convert()
+        self.background = pygame.transform.smoothscale(self.background, self.window.get_size())
+
         pygame.display.set_caption("No.of Collisions Simulator")
         
         self.clock = pygame.time.Clock()
@@ -16,7 +20,7 @@ class Game():
         self.running = True
         self.n_collisions = 0
         self.collision = 0
-        self.sound = pyglet.resource.media("clack_sound.mp3", streaming=False)
+        self.sound = pyglet.resource.media("clack.wav", streaming=False)
         self.font = pygame.font.Font("Minecraft.ttf",20)
         pygame.key.start_text_input()
 
@@ -24,9 +28,11 @@ class Game():
         self.v2 = -100
         self.m1 = 1
         self.m2 = 100
-        self.y = 300
-        self.pos1 = np.array([300,self.y])
-        self.pos2 = np.array([400,self.y])
+        self.y = 575
+        self.size1 = 50
+        self.size2 = 50 + (math.log(self.m2,10) * 70)
+        self.pos1 = np.array([225,self.y])
+        self.pos2 = np.array([370,self.y])
         self.Str_Mass = str(self.m2)
        
     def cal_pos(self,timestep):
@@ -46,6 +52,9 @@ class Game():
         self.v2 = (a + b) / c
         self.v1 = self.v2 + v2 - v1
 
+    def cycle_sim(self):
+        if self.pos2[0] > 880:
+            self.restart() 
     def update(self):
         if self.pos1[0]+50 >= self.pos2[0]:
             self.cal_vel()
@@ -54,7 +63,7 @@ class Game():
             else:
                 self.n_collisions = self.n_collisions+1
 
-        if 205 >= self.pos1[0]:
+        if 112 >= self.pos1[0]:
             self.v1 = -self.v1
             if self.timestep < 0:
                 self.n_collisions = self.n_collisions-1
@@ -69,30 +78,40 @@ class Game():
         self.v1 = 0
         self.v2 = -100
         self.m1 = 1
-        self.y = 300
-        self.pos1 = np.array([300,self.y])
-        self.pos2 = np.array([400,self.y])         
+        self.y = 575
+        self.size1 = 50
+        self.size2 = 50 + (math.log(self.m2,10) * 70)
+        self.pos1 = np.array([225,self.y])
+        self.pos2 = np.array([370,self.y])     
+        self.timestep = 1/2000    
 
     def UI_Render(self):
         #text for mass
         Text_Mass = 'Mass = ' 
         Text_Mass = str(Text_Mass + self.Str_Mass)
-        self.Text_Mass = self.font.render(Text_Mass, True, (250,250,250))
+        self.Text_Mass = self.font.render(Text_Mass, False, (250,250,250))
         self.rect = self.Text_Mass.get_rect()
         pygame.draw.rect(self.Text_Mass, (30,30,30), self.rect, 1)
-        self.window.blit(self.Text_Mass, (700, 100))
+        self.window.blit(self.Text_Mass, (1190, 100))
 
         #text for collisions
         self.Str_Collisions = str(self.n_collisions)
         Text_Collisions = 'Collsions = '
         Text_Collisions = str(Text_Collisions + self.Str_Collisions)
-        self.Text_Collisions = self.font.render(str(Text_Collisions), True, (250,250,250))
-        self.window.blit(self.Text_Collisions, (700, 20))
+        self.Text_Collisions = self.font.render(str(Text_Collisions), False, (250,250,250))
+        self.window.blit(self.Text_Collisions, (1190, 70))
         
+        #title text
+        self.Text_Title = self.font.render('Py Collision Simulator', False, (250,250,250))
+        rect1 = self.Text_Title.get_rect()
+        pygame.draw.rect(self.Text_Mass, (30,30,30), self.rect, 1)
+        pos_x = 683 - (rect1.width/2)
+        self.window.blit(self.Text_Title, (pos_x, 70))
+
         #ui for change mass
-        pos = (700+self.rect.topright[0],100+self.rect.topright[1])
+        pos = (1190+self.rect.topright[0],100+self.rect.topright[1])
         point = pygame.mouse.get_pos()
-        if point[0]>690 and point[0]<(710+self.rect.width) and point [1]>90 and point[1]<(110+self.rect.height):
+        if point[0]>1185 and point[0]<(1195+self.rect.width) and point [1]>95 and point[1]<(105+self.rect.height):
             cursor = pygame.Rect(pos, (7, self.rect.height))
             pygame.draw.rect(self.window, (250,250,250), cursor)
             self.cursor_touch = True
@@ -100,12 +119,12 @@ class Game():
             self.cursor_touch = False
             
     def render(self):
-        self.window.fill((40,40,40))
+        self.window.blit(self.background, (0, 0))
         #objects
-        pygame.draw.rect(self.window,(241, 76, 76),pygame.Rect(self.pos1[0],300,50,50))
-        pygame.draw.rect(self.window,(93, 102, 179),pygame.Rect(self.pos2[0],300,50,50))
-        pygame.draw.rect(self.window,(161, 216, 187),pygame.Rect(200,100,5,250))
-        pygame.draw.rect(self.window,(161, 216, 187),pygame.Rect(200,350,800,5))
+        pygame.draw.rect(self.window,(39, 40, 34),pygame.Rect(self.pos1[0],self.y,self.size1,self.size1))
+        pygame.draw.rect(self.window,(93, 102, 179),pygame.Rect(self.pos2[0],625-self.size2,self.size2,self.size2))
+        pygame.draw.rect(self.window,(200, 200, 200),pygame.Rect(105,110,7,522),0,10)
+        pygame.draw.rect(self.window,(200, 200, 200),pygame.Rect(105,625,1154,7),0,10)
         #UI
         self.UI_Render()
         #update display
@@ -116,6 +135,7 @@ class Game():
         while self.running:
             for i in range(10):
                 self.update()
+            self.cycle_sim()
             self.render()        
             self.clock.tick(self.fps)
             if self.n_collisions != self.collision:
@@ -130,12 +150,12 @@ class Game():
                     break
                 elif event.type == pygame.KEYDOWN:
                     #timestep controls
-                    if event.key == pygame.K_RIGHT:
+                    if event.key == pygame.K_LEFT:
                         if self.timestep < 0:
                             self.timestep = 0
                         else:
                                 self.timestep = 1/2000
-                    elif event.key == pygame.K_LEFT:
+                    elif event.key == pygame.K_RIGHT:
                         if self.timestep > 0:
                             self.timestep = 0
                         else:
